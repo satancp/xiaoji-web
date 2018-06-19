4cea302458f64922c1bf62ca4763cd6c966e3457
Chris <zx603852402@gmail.com>
2018-06-19 22:35:56 +0800

    update

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 let configs = {
-    server_url: 'http://localhost:7001/api/'
-    // server_url: 'https://dashboard.goland.design/api/'
+    // server_url: 'http://localhost:7001/api/'
+    server_url: 'https://dashboard.goland.design/api/'
 };
 
 export default configs;
